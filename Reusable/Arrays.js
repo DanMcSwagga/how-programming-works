2079a6838c1d956023e5bf0a2022806d5875326b
Dan Stetsenko <denyast@ukr.net>
2019-09-10 21:31:29 +0300

    Adjusted findPhoneByName function, as corrected by eng version of the task.

--- Reusable/Arrays.js
+++ Reusable/Arrays.js
@@ -3,7 +3,8 @@
 2. Реализуйте функцию `findPhoneByName` с сигнатурой
 `findPhoneByName(name: string): string`.
 При вызове функция должна находить объект, где поле `name` равно аргументу
-`name` и возвращать номер телефона из объекта. */
+`name` и возвращать номер телефона из объекта.
+Использовать for loop для этого поиска. */
 
 'use strict';
 
@@ -13,7 +14,12 @@ const phonebook = [
   { name: 'Jamie Siminoff', phone: '+38917412XXXX' }
 ];
 
-const findPhoneByName = name =>
-  phonebook.find(record => name === record.name).phone;
+const findPhoneByName = name => {
+  // phonebook.find(record => name === record.name).phone;
+  for (const record of phonebook) {
+    if (record.name === name) return record.phone;
+  }
+  return 'Phone not found.';
+};
 
 console.log(findPhoneByName('Dan Stetsenko'));
